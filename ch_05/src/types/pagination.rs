a07d1f76717e89cb807a5d2e5343bcb2ee77900b
root <rnarkkx@gmail.com>
2023-04-21 04:15:48 +0000

    Update

--- ch_05/src/types/pagination.rs
+++ ch_05/src/types/pagination.rs
@@ -28,19 +28,19 @@ pub struct Pagination {
 /// assert_eq!(p.start, 1);
 /// assert_eq!(p.end, 10);
 /// ```
-pub fn extract_pagination(params: HashMap<String, String>) 
+pub fn extract_pagination(params: HashMap<String, String>)
     -> Result<Pagination, Error> {
         // Could be improved in the future
         if params.contains_key("start") && params.contains_key("end") {
             return Ok(Pagination {
-                // Takes the "start" parameter in the query 
+                // Takes the "start" parameter in the query
                 // and tries to convert it to a number
                 start: params
                     .get("start")
                     .unwrap()
                     .parse::<usize>()
                     .map_err(Error::ParseError)?,
-                // Takes the "end" parameter in the query 
+                // Takes the "end" parameter in the query
                 // and tries to convert it to a number
                 end: params
                     .get("end")
