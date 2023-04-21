ff5de6778d19b1e0bb1440832ed7eb5df3c880be
root <rnarkkx@gmail.com>
2023-04-21 10:34:28 +0000

    Update

---
+++ ch_11/axum/src/routes/question.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+
+use axum::{
+    http::StatusCode,
+    response::Json,
+};
+use tracing::{event, instrument, Level};
+
+use crate::profanity::check_profanity;
+use crate::store::Store;
+use crate::types::account::Session;
+use crate::types::pagination::{extract_pagination, Pagination};
+use crate::types::question::{NewQuestion, Question};
+
+#[instrument]
+pub async fn get_questions(
+    Query(pagination): Query<Option<Pagination>>,
+    store: Store,
+) -> Result<Json<Vec<Question>>> {
+    event!(target: "practical_rust_book", Level::INFO, "querying questions");
+    let Pagination { limit, offset } = match pagination.0 {
+        Some(pagination) => {
+            event!(Level::INFO, pagination = true);
+            pagination
+        }
+        None => Pagination::default()
+    };
+
+    match store.get_questions(limit, offset).await {
+        Ok(res) => Ok(Json(&res)),
+        Err(e) => Err(e),
+    }
+}
+
+pub async fn update_question(
+    id: i32,
+    session: Session,
+    store: Store,
+    question: Question,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let account_id = session.account_id;
+    if store.is_question_owner(id, &account_id).await? {
+        let title = check_profanity(question.title);
+        let content = check_profanity(question.content);
+
+        let (title, content) = tokio::join!(title, content);
+
+        if title.is_ok() && content.is_ok() {
+            let question = Question {
+                id: question.id,
+                title: title.unwrap(),
+                content: content.unwrap(),
+                tags: question.tags,
+            };
+            match store.update_question(question, id, account_id).await {
+                Ok(res) => Ok(warp::reply::json(&res)),
+                Err(e) => Err(warp::reject::custom(e)),
+            }
+        } else {
+            Err(warp::reject::custom(
+                title.expect_err("Expected API call to have failed here"),
+            ))
+        }
+    } else {
+        Err(warp::reject::custom(handle_errors::Error::Unauthorized))
+    }
+}
+
+pub async fn delete_question(
+    id: i32,
+    session: Session,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let account_id = session.account_id;
+    if store.is_question_owner(id, &account_id).await? {
+        match store.delete_question(id, account_id).await {
+            Ok(_) => Ok(warp::reply::with_status(
+                format!("Question {} deleted", id),
+                StatusCode::OK,
+            )),
+            Err(e) => Err(warp::reject::custom(e)),
+        }
+    } else {
+        Err(warp::reject::custom(handle_errors::Error::Unauthorized))
+    }
+}
+
+pub async fn add_question(
+    session: Session,
+    store: Store,
+    new_question: NewQuestion,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let account_id = session.account_id;
+    let title = match check_profanity(new_question.title).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let content = match check_profanity(new_question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let question = NewQuestion {
+        title,
+        content,
+        tags: new_question.tags,
+    };
+
+    match store.add_question(question, account_id).await {
+        Ok(question) => Ok(warp::reply::json(&question)),
+        Err(e) => Err(warp::reject::custom(e)),
+    }
+}
---
+++ ch_11/axum/src/types/account.rs
@@ -0,0 +1,18 @@
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Session {
+    pub exp: DateTime<Utc>,
+    pub account_id: AccountId,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Account {
+    pub id: Option<AccountId>,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct AccountId(pub i32);
