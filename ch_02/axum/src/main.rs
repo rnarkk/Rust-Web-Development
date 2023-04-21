dc1825c8768c1370d5e9a7906e61827cc45e7814
root <rnarkkx@gmail.com>
2023-04-21 08:14:47 +0000

    Update

---
+++ ch_02/axum/src/main.rs
@@ -0,0 +1,52 @@
+use std::{
+    io::{Error, ErrorKind},
+    str::FromStr
+};
+use axum::{
+    Router, Server,
+    routing::get
+};
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { format!("Hello, World!") }));
+
+    Server::bind(&"127.0.0.1:3030".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
