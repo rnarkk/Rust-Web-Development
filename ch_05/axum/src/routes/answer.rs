35f44407441168915a3f974eca1273518ef64e5b
Rin Arakaki <rnarkkx@gmail.com>
2023-04-21 23:47:14 +0900

    Update answer.rs

--- ch_05/axum/src/routes/answer.rs
+++ ch_05/axum/src/routes/answer.rs
@@ -1,23 +1,30 @@
-use std::collections::HashMap;
-use axum::http::StatusCode;
+use std::{
+    collections::HashMap,
+    sync::Arc
+};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse
+};
 
-use crate::store::Store;
-use crate::types::{
-    answer::{Answer, AnswerId},
-    question::QuestionId,
+use crate::{
+    store::Store,
+    types::{
+        answer::{Answer, AnswerId},
+        question::QuestionId,
+    }
 };
 
 pub async fn add_answer(
-    store: Store,
     params: HashMap<String, String>,
-) -> Result<impl warp::Reply, warp::Rejection> {
+    State(store): State<Arc<Store>>
+) -> impl IntoResponse {
     let answer = Answer {
         id: AnswerId("1".to_string()),
         content: params.get("content").unwrap().to_string(),
         question_id: QuestionId(params.get("questionId").unwrap().to_string()),
     };
-
     store.answers.write().await.insert(answer.id.clone(), answer);
-
     Ok(warp::reply::with_status("Answer added", StatusCode::OK))
 }
